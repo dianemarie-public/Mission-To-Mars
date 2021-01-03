f9b52058aaa744e800d4f7d7006cd53908758f19
dianemarie-public <69361953+dianemarie-public@users.noreply.github.com>
2021-01-03 14:00:03 -0600

    Update app.py

--- MIssions_to_Mars/app.py
+++ MIssions_to_Mars/app.py
@@ -11,26 +11,27 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
-def index():
+def home():
+    
     # Find one record of data from the mongo database
-    mars = mongo.db.collection.find_one()
+    mars_data = mongo.db.collection.find_one()
+    
     # Return template and data
     return render_template("index.html", 
-    mars=mars)
+    mars=mars_data)
 
 # Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
-    mars = mongo.db.mars
+
     # Run the scrape function
-    data = scrape_mars.scrape()
-    mars.update(
-        {},
-        data,
-        upsert=True
-    )
+    news_data = scrape_mars.scrape_info()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, news_data, upsert=True)
+
     # Redirect back to home page
-    return redirect("/", code=302)
+    return redirect("/")
 
 
 if __name__ == "__main__":
