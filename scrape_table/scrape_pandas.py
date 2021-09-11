9cdaa4fa64f392f4e52bd59d4f64aef6b9667daf
dianewitt <dianemarieak@gmail.com>
2021-09-11 16:11:48 -0500

    Update scrape_pandas.py

--- scrape_table/scrape_pandas.py
+++ scrape_table/scrape_pandas.py
@@ -1,36 +1,37 @@
 import pandas as pd
-import requests
-from bs4 import BeautifulSoup as bs
 
-url = "https://tax.alaska.gov/programs/oil/production/ans.aspx?"
+# found earliest data available (should scrape from dropdown menu link below)
+# https://tax.alaska.gov/programs/oil/production.aspx?10034
 month = '1'
 month
-year = '2021'
+year = '2002'
 year
 day = '1'
+start_url = url + str(month) + '/' + str(day) + '/' + str(year)
+start_url
 
-url = url + str(month) + '/' + str(day) + '/' + str(year)
-url
+
 # -----------------------------
 # set the url for the most recent page update
-# url = "https://tax.alaska.gov/programs/oil/production/ans.aspx?9/1/2021"
-tables = pd.read_html(url)
+end_url = "https://tax.alaska.gov/programs/oil/production/ans.aspx?9/1/2021"
+tables = pd.read_html(end_url)
 type(tables)
 df = tables[6]
 df = df.loc[2:]
 df = df.reset_index(drop=True)
 
-
+# delete extra columns
 del df[8]
 del df[9]
 del df[10]
 del df[11]
 del df[12]
 
+# rename columns (for Alaska North Slope oil field names)
 df = df.rename(columns = {0:'date', 1:'prudhoe', 2:'kaparuk', 3:'endicott', 4:'lisburne', 5:'alpine', 6:'ans', 7:'inventories'})
 df.columns
-# df.head()
-df['prudhoe'].dtype
+
+# convert strings of html data to numeric (float)
 df['prudhoe'] = pd.to_numeric(df['prudhoe'],errors='coerce')
 df['kaparuk'] = pd.to_numeric(df['kaparuk'],errors='coerce')
 df['endicott'] = pd.to_numeric(df['endicott'],errors='coerce')
@@ -38,7 +39,7 @@ df['lisburne'] = pd.to_numeric(df['lisburne'],errors='coerce')
 df['alpine'] = pd.to_numeric(df['alpine'],errors='coerce')
 df['ans'] = pd.to_numeric(df['ans'],errors='coerce')
 df['inventories'] = pd.to_numeric(df['inventories'],errors='coerce')
-# df.head()
-# df
+
+# drop any rows with NaN
 df = df.dropna(how='any')
 df
