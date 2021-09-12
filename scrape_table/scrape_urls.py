aa0d022f7eaec1a5ce3ee11d38a2d7aef9dcb96e
dianewitt <dianemarieak@gmail.com>
2021-09-12 18:03:25 -0500

    Update scrape_urls.py

--- scrape_table/scrape_urls.py
+++ scrape_table/scrape_urls.py
@@ -3,27 +3,41 @@ url = "https://tax.alaska.gov/programs/oil/production/ans.aspx?"
 html = get(url)
 soup = Soup(html)
 
-months = []
-years = []
-
-# interactive test code
-soup.find('option')[1:]
-
 list = soup.find('option')[1:]
 length = len(list)
-length
 
-# interactive test code
-list[0].attrs['value']
+# base_url = "https://tax.alaska.gov/programs/oil/production/ans.aspx?"
+urls = []
 
-options = []
-
-# iterate over the reset_index same as 'for i in range(len(list))'
 for i in range(length):
-    option = list[i].attrs['value']
-    url = "https://tax.alaska.gov/programs/oil/production/ans.aspx?"
-    options.append(url + option)
-    # print(option)
-    # print(list[i].attrs['value'])
+    options = list[i].attrs['value']
+    urls.append(url + options)
 
-options
+len(urls)
+
+import pandas as pd
+
+data = []
+
+# df = df.rename(columns = {0:'date', 1:'prudhoe', 2:'kaparuk', 3:'endicott', 4:'lisburne', 5:'alpine', 6:'ans', 7:'inventories'})
+# df.columns
+# df.reset_index()
+
+for url in urls:
+    df = pd.read_html(url)[6]
+    df = df.loc[2:]
+    df[1] = pd.to_numeric(df[1],errors='coerce')
+    df[2] = pd.to_numeric(df[2],errors='coerce')
+    df[3] = pd.to_numeric(df[3],errors='coerce')
+    df[4] = pd.to_numeric(df[4],errors='coerce')
+    df[5] = pd.to_numeric(df[5],errors='coerce')
+    df[6] = pd.to_numeric(df[6],errors='coerce')
+    df[7] = pd.to_numeric(df[7],errors='coerce')
+    del df[8]
+    del df[9]
+    del df[10]
+    del df[11]
+    del df[12]
+    df = df.dropna(how='any')
+    df = df[df[0] != 'Average']
+    df.to_csv('pandas.csv', mode='a', header=False, index=False)
